62593041b9cae645c7e56ab863d745750da93d8e
yurii <urakarabin123@gmail.com>
2022-08-20 15:29:45 +0300

    Initial commit
    Create architecture for my app

---
+++ Blog.Domain/Article.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Domain
+{
+    public class Article : BaseModel
+    { 
+        public Guid UserId { get; set; }
+        public string Title { get; set; } = String.Empty;
+        public string Content { get; set; } = String.Empty;
+        public bool VerifyStatus { get; set; }
+        public bool isDeleted { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ Blog.Domain/BaseModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Domain
+{
+    public class BaseModel
+    {
+        public Guid Id { get; set; }
+        public Guid CreatedBy { get; set; }
+        public Guid UpdatedBy { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public DateTime UpdatedTime { get; set; }
+
+    }
+}
